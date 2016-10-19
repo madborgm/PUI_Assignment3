2aba83199c718aed15147f834a4a11454f24e12e
Madison Borgmann <madisonborgmann@Madisons-MacBook-Pro.local>
2016-10-19 16:27:21 -0400

    Changes made for assignment 4

--- js/script.js
+++ js/script.js
@@ -53,3 +53,130 @@ function newGift() {
 	}
 
 }
+
+function saveCategoryName(){
+	var groupCategoryInput = document.getElementById("groupCategory");
+
+	localStorage.setItem("groupCategoryName", groupCategoryInput.value);
+	var groupBudgetInput = document.getElementById("groupBudget");
+	localStorage.setItem("groupBudgetValue", groupBudgetInput.value);
+
+	groupCategoryInput.value = "";
+	groupBudgetInput.value = "";
+}
+
+function getCategoryName(){
+	var groupCategoryHeader = localStorage.getItem("groupCategoryName");
+	var groupBudgetHeader = localStorage.getItem("groupBudgetValue");
+	
+	var groupName = document.createElement("LI");
+	var nameText = document.createTextNode(groupCategoryHeader);
+		groupName.appendChild(nameText);
+	
+	var budget = document.createElement("LI"); 
+	var number = document.createTextNode(groupBudgetHeader);
+		budget.appendChild(number);      
+
+	
+	var link = document.createElement('a');
+		link.setAttribute('href', "groups.html");
+	var goToArrow = document.createElement("LI");
+	var goToIcon = document.createElement("I");
+	goToIcon.className = "fa fa-chevron-circle-right"
+	goToArrow.appendChild(goToIcon);
+	link.className = "groupsLink";
+	link.appendChild(goToArrow);
+
+
+	list = document.getElementById("groupNamesList");
+	list.appendChild(groupName);
+	// list.appendChild(budget);
+	list.appendChild(link);
+
+	
+	var groupDiv = document.getElementById("groupNames");
+	groupDiv.appendChild(list);
+
+}
+
+$(document).ready(function(){
+    $("#addCategory").click(function(){
+        $("#addMemberForm").hide();
+    });
+    $("#addMember").click(function(){
+    	$("#addMemberForm").toggle();
+    });
+    $("#addCategory").click(function(){
+    	$("i").removeClass("fa-minus-circle").addClass("fa-plus-circle");
+    });
+	$("i.fa-plus-circle, i.fa-minus-circle").click(function() {
+	    var $this = $(this);
+	    if ($this.hasClass("fa-plus-circle")) {
+	         $this.removeClass("fa-plus-circle").addClass("fa-minus-circle");
+	    }
+	    else {
+	         $this.removeClass("fa-minus-circle").addClass("fa-plus-circle");
+	    }
+	});
+	$("#editMember").click(function(){
+		$("#addGroupMemberForm").toggleClass("addGroupMemberList");
+	});
+
+	$("#addGift").click(function(){
+		$("#addGroupMemberForm").addClass("addGroupMemberList");
+
+
+	});
+});
+
+function loadGivenGroupNames() {
+        var groupCategoryHeader = localStorage.getItem("groupCategoryName");
+		var groupBudgetHeader = localStorage.getItem("groupBudgetValue");
+
+		var groupName = document.createElement("LI");
+		var nameText = document.createTextNode(groupCategoryHeader);
+			groupName.appendChild(nameText);
+	
+		var budget = document.createElement("LI"); 
+		var number = document.createTextNode(groupBudgetHeader);
+			budget.appendChild(number);
+
+		list = document.getElementById("groupGivenNamesList");
+		list.appendChild(groupName);
+		list.appendChild(budget);    
+    };
+window.onload = loadGivenGroupNames;
+
+function saveGroupMemberName() {
+	var groupMemberFirstName = document.getElementById("groupMemberFName");
+	localStorage.setItem("memberFirstName", groupMemberFirstName.value);
+
+	var groupMemberLastName = document.getElementById("groupMemberLName");
+	localStorage.setItem("memberLastName", groupMemberLastName.value);
+
+	groupMemberFirstName.value = "";
+	groupMemberLastName.value = "";
+}
+
+function getGroupMemberName() {
+	var firstName = localStorage.getItem("memberFirstName");
+	var lastName = localStorage.getItem("memberLastName");
+
+	var fullName = document.createElement("LI");
+	var fullNameText = document.createTextNode(firstName + " " + lastName);
+	fullName.appendChild(fullNameText);
+
+	namesList = document.getElementById("memberNamesLinkList");
+	namesList.appendChild(fullName);
+
+}
+
+function changeButton() {
+	document.getElementById("editMember").innerHTML = "Cancel";
+}
+
+function changeButtonBack() {
+	document.getElementById("editMember").innerHTML = "Edit Member";
+}
+
+
--- js/showHide.js
+++ js/showHide.js
@@ -2,5 +2,4 @@ $(document).ready(function(){
     $("button").click(function(){
         $("p").removeClass("intro");
     });
-    
 });
